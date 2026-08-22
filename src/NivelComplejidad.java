e2e3defdc7dd92a4c4742eadffe4fda0848cd9e4
David Estrada <david_edgardo@unitec.edu>
2026-08-22 08:54:29 -0600

    Clase enum completa

---
+++ src/NivelComplejidad.java
@@ -0,0 +1,22 @@
+public enum NivelComplejidad {
+    BAJO(1,0,"Nivel de complejidad Bajo"),
+    MEDIO(2,3,"Nivel de complejidad Medio"),
+    ALTO(3,6,"Nivel de complejidad Alto");
+
+    private final int ordenComplejidad;
+    private final int diasAdicionales;
+    private final String nivelComplejidad;
+    NivelComplejidad(int ordenComplejidad, int diasAdicionales, String nivelcomplejidad){
+        this.ordenComplejidad=ordenComplejidad;
+        this.diasAdicionales=diasAdicionales;
+        this.nivelComplejidad=nivelcomplejidad;
+    }
+
+    public boolean requiereAutorizacionEspecial(){
+        if (this == ALTO){
+            return true;
+        } return false;
+    }
+
+
+}
